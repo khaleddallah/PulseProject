d8e8136f5dfbbcf816d7219d0b97555c6e402384
khaled.dallah0@gmail.com <khaled.dallah0@gmail.com>
2019-01-28 14:24:26 +0200

    Add SearchName and SearchId

--- appa.py
+++ appa.py
@@ -1,24 +1,34 @@
 import sys
 from PyQt5.QtCore import pyqtSlot 
-from PyQt5.QtWidgets import QApplication,QMainWindow,QWidget,QStackedWidget,QTableWidgetItem
-from PyQt5.uic import loadUi
-#from DBfunctions import *
 
-class Main(QStackedWidget):
-# class Main(QStackedWidget,DBMain):
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow,QWidget
+from PyQt5.QtWidgets import QStackedWidget,QTableWidgetItem
+from PyQt5.QtWidgets import QHeaderView,QAbstractItemView
+
+from PyQt5.QtGui import QIntValidator
+
+from PyQt5.uic import loadUi
+
+from warf_files.funktions2 import *
+
+
+#===========================================================================
+class Main(QStackedWidget,funktions):
 	def __init__(self):
 		super().__init__()
+		funktions.ConnectToDB(r"./warf_files/database3.db")
 		loadUi('s1.ui',self)
 		self.loadMain()
 
-	#0
+	#=============================== 0
 	def loadMain(self):
 		self.setCurrentIndex(0)
 		self.PatientsDB0.clicked.connect(self.loadPatientsDB)
 		self.PulseRecording0.clicked.connect(self.loadPulseRecordingC)
 		self.MedicalImagesProcessing0.clicked.connect(self.loadMedImgProcessingC)
 
-	#1h
+	#=============================== 1
 	def loadPatientsDB(self):
 		self.setCurrentIndex(1)
 		
@@ -29,82 +39,124 @@ class Main(QStackedWidget):
 		self.AddMedicalImage1.clicked.connect(self.loadAddMedImg)
 
 		#Search button (get the first name and last name fields)
-		self.Search1.clicked.connect(self.SearchF1)
-		self.Delete1.clicked.connect(self.insertInTable)
+		self.Search1.clicked.connect(self.SearchByName)
+		self.SearchById1.clicked.connect(self.SearchById)
+
+		#self.Delete1.clicked.connect(self.insertInPatientsTableWidget)
 
 		#Table config
+		#resize table header to fit the word of fields
+		self.tableWidget1.resizeColumnsToContents()
+		#to make user able to select complete row by click any where of it 
+		self.tableWidget1.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+
+		#To allow only int
+		self.onlyInt = QIntValidator()
+		self.IdBox1.setValidator(self.onlyInt)
 
 
 
-	def SearchF1(self):
-		fn1=self.FirstNameBox1.text()
-		ln1=self.LastNameBox1.text()
-		print(fn1+' '+ln1)
+	def SearchById(self):
+		print('...searchByID Running')
+		patId=int(self.IdBox1.text())
+		data=funktions.sucheID(patId)
+		print(data)
+		self.insertMultiRowInPatientsTable(data)
 
-	def insertInTable (self):
+
+	def SearchByName(self):
+		print('...searchByName Running')
+		firstName=self.FirstNameBox1.text()
+		lastName=self.LastNameBox1.text()
+		data=funktions.sucheName(firstName,lastName)
+		print(data)
+		self.insertMultiRowInPatientsTable(data)
+
+
+	def insertMultiRowInPatientsTable (self,m):
+		self.tableWidget1.setRowCount(0);
+		for i in m:
+			self.insertOneRowInPatientsTable(i)
+		
+
+	def insertOneRowInPatientsTable (self,x):
 		numRows = self.tableWidget1.rowCount()
 		self.tableWidget1.insertRow(numRows)
+		self.tableWidget1.setItem(0,0, QTableWidgetItem(x[0])) #id
+		self.tableWidget1.setItem(0,1, QTableWidgetItem(x[0])) #first
+		self.tableWidget1.setItem(0,2, QTableWidgetItem(x[0])) #last
+		self.tableWidget1.setItem(0,3, QTableWidgetItem(x[0])) #fater
+		self.tableWidget1.setItem(0,4, QTableWidgetItem(x[0])) #lastVisit
 
-		self.tableWidget1.setItem(0,0, QTableWidgetItem("1"))
-		self.tableWidget1.setItem(0,1, QTableWidgetItem("Khaled"))
-		self.tableWidget1.setItem(0,2, QTableWidgetItem("Dallah"))
-		self.tableWidget1.setItem(0,3, QTableWidgetItem("EzzAlden"))
-		self.tableWidget1.setItem(0,4, QTableWidgetItem("12-2-2019"))
 
 
-	#2
+
+
+	#=============================== 2
 	def loadNewPatient(self):
 		self.setCurrentIndex(2)
 		self.Back2.clicked.connect(self.loadPatientsDB)
 		self.Main2.clicked.connect(self.loadMain)
+		self.Add2.clicked.connect(self.AddToPatientsTableDB)
 
-	#3
+
+	def AddToPatientsTableDB(self):
+		patId=funktions.CountOfPatientsTable()+1
+		firstName=self.FirstNameBox2.text()
+		lastName=self.LastNameBox2.text()
+		father=self.FatherNameBox2.text()
+		BirthDate=self.BirthDateBox2.text()
+		Data=[patId,firstName,lastName,father,BirthDate]
+		funktions.neuPat(Data)
+
+	#=============================== 3
 	def loadNewPatientS(self):
 		self.setCurrentIndex(3)
 		self.Back5.clicked.connect(self.loadNewPatient)
 		self.Main5.clicked.connect(self.loadMain)
 
 
-	#4
+	#=============================== 4
 	def loadNewPatientE(self):
 		self.setCurrentIndex(4)
 		self.Back5.clicked.connect(self.loadNewPatient)
 		self.Main5.clicked.connect(self.loadMain)
 
 
-	#5
+	#=============================== 5
 	def loadViewPatient(self):
 		self.setCurrentIndex(5)
 		self.Back5.clicked.connect(self.loadPatientsDB)
 		self.Main5.clicked.connect(self.loadMain)
 
-	#6
+	#=============================== 6
 	def loadAddMedImg(self):
 		self.setCurrentIndex(6)
 		self.Back6.clicked.connect(self.loadPatientsDB)
 		self.Main6.clicked.connect(self.loadMain)
 
-	#9
+	#=============================== 9
 	def loadPulseRecordingC(self):
 		self.setCurrentIndex(9)
 		self.Back9.clicked.connect(self.loadMain)
 		self.Next9.clicked.connect(self.loadPulseRecordingM)
 
 
-	#10
+	#=============================== 10
 	def loadPulseRecordingM(self):
 		self.setCurrentIndex(10)
 		self.Back10.clicked.connect(self.loadPulseRecordingC)
 		self.Main10.clicked.connect(self.loadMain)
 
-	#7
+	#=============================== 7
 	def loadMedImgProcessingC(self):
 		self.setCurrentIndex(7)
 		self.Back7.clicked.connect(self.loadMain)
 		self.Next7.clicked.connect(self.loadMedImgProcessingM)
 
 
-	#8
+	#=============================== 8
 	def loadMedImgProcessingM(self):
 		self.setCurrentIndex(8)
 		self.Back8.clicked.connect(self.loadMedImgProcessingC)
--- warf_files/funktions2.py
+++ warf_files/funktions2.py
@@ -1,107 +1,89 @@
 import sqlite3
 
-connection = sqlite3.connect(r"./database3.db")
-cur1 = connection.cursor()
+#connection = sqlite3.connect(r"./database3.db")
+#cur1 = connection.cursor()
 
-class funktions():
-       def DBdatenanzeigen():#لإظهار كل المعلومات الموجودة في db
+class funktions:
+       curl=0
+       def ConnectToDB(path):
+           connection = sqlite3.connect(path)
+           funktions.cur1 = connection.cursor()
+
+       def CountOfPatientsTable():
+           command="""SELECT count(*) FROM Patient"""
+           funktions.cur1.execute(command)
+           infos=funktions.cur1.fetchall()
+           return(infos[0][0])
+
+
+       def DBdatenanzeigen():
            command="""select Patient.PatID,Patient.VORNAME,Patient.NACHNAME,Patient.GEBURTSDATUM,Patient.LETZTENBESUCH,BILDER.untersuchungstyp,BILDER.Bildgebungdatum,BILDER.SPEICHERORT from Patient left join Bilder on Patient.PatID=Bilder.PatID """
-           cur1.execute(command)
-           infos=cur1.fetchall()
+           funktions.cur1.execute(command)
+           infos=funktions.cur1.fetchall()
            print(infos)
 
 
-       def neuPat(pid=0, x="S", y="Z", z="k", l="m"):#إضافة مريض جديد الى الdb
-           pid = int(input("Bitte geben sie die PatientID ein"))#الرجاء ادخال رقم المريض
-           x = input("Bitte geben Sie die Vorname des Patients ein")#الرجاء ادخال الاسم الاول للمريض
-           y = input("Bitte geben Sie die Nachname des Patients ein")#الرجاء ادخال الاسم الثاني (الكنية) للمريض
-           z = input("Bitte geben Sie das Geburtsdatum des Patients ein")#الرجاء ادخال تاريخ ميلاد المريض
-           l = input("Bitte geben Sie das Datum des letztenversuch ,wie hier(14 uhr 12.10.2018),ein") #الرجاء ادخال تاريخ اخر زيارة كما هو موضح في الشكل
-           neu=(pid,x,y,z,l)
+       def neuPat(x):
            command1 = """INSERT INTO Patient VALUES(?,?,?,?,?)"""
-           cur1.execute(command1,neu)
-           connection.commit()
+           funktions.cur1.execute(command1,x)
+           #connection.commit()
            command2="""select * from Patient"""
-           cur1.execute(command2)
-           infos=cur1.fetchall()
+           funktions.cur1.execute(command2)
+           infos=funktions.cur1.fetchall()
            print(infos)
 
-       def löschen():#لحذف بيانات مريض
+       def loschen(id):
+           command3="""delete  from Patient where PatID = {}""".format(id)
+           command4="""delete from BILDER where PatID={}""".format(id)
+           funktions.cur1.execute(command3)
+           funktions.cur1.execute(command4)
+           connection.commit()
 
-           while True:
-               x = int(input("Wählen Sie bitte aus..1 für löschen nach Name,2 löscheln nach nummer,3 die  gesamte Tabele leeren, die andere Auswahlen beenden die Prozess" ))#الرجاء اختيار الرقم 1 للحذف عن طريق الاسم و2 للحذف عن طريق رقم المريض
+       def sucheID(x=-1):
+           if (x== -1):
+               print('Error ID')
+               return(-1)
+           command1="""select * from Patient where PatID=(?)"""
+           print(type(x))
+           s=str(x)
+           funktions.cur1.execute(command1,s)
+           pat1=funktions.cur1.fetchall()
+           return(pat1)
 
-               if x==1:# الحذف بناءا على الاسم
-                   name=input("Geben Sie die Name des Patients,dessen Daten Sie aus DB löschen möchten")#الرجاء ادخال اسم المريض الذي تريد حذف بياناته
-                   x="""select PatID from Patient where Patient.VORNAME='{}' """.format(name)
-                   command1="""delete  from Patient where VORNAME='{}' """.format(name)
-                   command2="""delete from BILDER where PatID=?"""
-                   cur1.execute(command1)
-                   cur1.execute(command2,x)
-                   connection.commit()
+       def sucheName(x='0',y='0'):
+           if (x=='0' and y=='0'):
+               print('Error Name')
+               return('0')
+
+           elif (y=='0'):
+               command2 = """select * from Patient where VORNAME =(?)"""
+               funktions.cur1.execute(command2,(x,))
+               pat2 = funktions.cur1.fetchall()
+               return(pat2)
+
+           else:
+               command2 = """select * from Patient where VORNAME =(?) and NACHNAME =(?)"""
+               funktions.cur1.execute(command2,(x,y))
+               pat2 = funktions.cur1.fetchall()
+               return(pat2)
 
 
-               elif (x==2):#الحذف بناءا على رقم المريض
-                   id=input("Geben Sie das ID des Patient,dessen Daten Sie aus DB löschen möchten")
-                   command3="""delete  from Patient where PatID = {}""".format(id)
-                   command4="""delete from BILDER where PatID={}""".format(id)
-                   cur1.execute(command3)
-                   cur1.execute(command4)
-                   connection.commit()
-               elif (x==3):
-                   command5="""delete  from Patient """
-                   command6="""delete from BILDER"""
-                   cur1.execute(command5)
-                   cur1.execute(command6)
-                   connection.commit()
-               else:#واذا تم ادخال رقم اخر يقوم بعرض البيانات واغلاق التابع
-                   command7 = """select * from Patient left join Bilder on Patient.PatID=Bilder.PatID"""
-                   cur1.execute(command7)
-                   infos = cur1.fetchall()
-                   print(infos)
-                   print("Die prozess wurde beendet")
-
-                   break
-       def suche(x=1):#للبحث عن مريض معين في قاعدة البيانات
-
-           while True:
-               wahl=int(input("MÖCHTEN Sie nach ID oder nach Vorname des Patients suchen ,bitte drücken sie 1 für ID oder 2 für Vorname ,die andere Auswahlen beenden die Prozess" ))#اتريد البحث بناءا على الرقم ام الاسم الاول للمريض  الرجاء اضغط 1 للبحث عن طريق الرقم و2 للأسم والخيارات الاخرى تنهي العملية
-               if (wahl ==1):#للبحث عن طريق الرقم
-                   x=input("Geben Sie bitte die Nummer des angegraften Patients ein")#الرجاء ادخال رقم المريض المطلوب
-                   command1="""select * from Patient where PatID is ?"""
-                   cur1.execute(command1,x)
-                   pat1=cur1.fetchall()
-                   print(pat1)
-
-               elif (wahl==2):#للبحث عن طريق الاسم
-
-                   x=input("Geben Sie bitte die Vorname des angegraften Patients ein")#الرجاء ادخال الاسم الاول للمريض المطلوب
-                   command2 = """select * from Patient where VORNAME =(?)"""
-
-                   cur1.execute(command2,(x,))
-                   pat2 = cur1.fetchall()
-                   print(pat2)
-
-               else:#في حال تم ادخال رقم اخر يتم انهاء العملية
-
-                   print("Die Prozess wurde beendet")#انهيت العملية
-                   break
-       def bilderhinfügen(PatID=0 ,bildtyp= "n", Datum= "n", speicherort= "n"):#لإضافة صورة طبية الى قاعدة البيانات
-           patid=input("Geben Sie bitte die Nummer der Patient ein")#الرجاء ادخال رقم المريض
-           x=input("WAS IST DER ART DER UNTERSUCHUNG ,X-Ray,Ultraschall(Sonografie),Szintigrafie(GamaKamera),Endoskopie,Computertomografie,MRT oder Elektrokardiogramm EKG")#ما نوع الصورة الطبية .......
-           y=input("Wann wurde die Bildgebung stattgefunden")#متى تم التصوير
-           z=input("Welsches Bild möchten Sie hinfügen")#اي صورة تريد اضافتها (هون منضيف عنوان الصورة)
+       def bilderhinfügen(PatID=0 ,bildtyp= "n", Datum= "n", speicherort= "n"):
+           patid=input("Geben Sie bitte die Nummer der Patient ein")
+           x=input("WAS IST DER ART DER UNTERSUCHUNG ,X-Ray,Ultraschall(Sonografie),Szintigrafie(GamaKamera),Endoskopie,Computertomografie,MRT oder Elektrokardiogramm EKG")
+           y=input("Wann wurde die Bildgebung stattgefunden")
+           z=input("Welsches Bild möchten Sie hinfügen")
            command1="""INSERT INTO BILDER VALUES(?,?,?,?)"""
            bilder1=(patid,x,y,z)
-           cur1.execute(command1,bilder1)
+           funktions.cur1.execute(command1,bilder1)
            connection.commit()
 
 
 
-funktions.DBdatenanzeigen()
-funktions.neuPat()
-funktions.bilderhinfügen()
-funktions.löschen()
-connection.close()
+# funktions.DBdatenanzeigen()
+# funktions.neuPat()
+# funktions.bilderhinfügen()
+# funktions.löschen()
+# connection.close()
 
 ###funktions.suche()
